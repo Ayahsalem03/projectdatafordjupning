61c3224c741392d2b75b8e4b444f34943b99c9e4
Ayahsalem03 <aysa0005@student.hv.se>
2025-11-10 18:31:21 +0100

    Add files via upload

---
+++ udp_video_sim.cc
@@ -0,0 +1,98 @@
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/flow-monitor-module.h"
+
+using namespace ns3;
+
+int main(int argc, char *argv[]) {
+  double simTime = 10.0;
+  uint32_t packetSize = 1200;
+  double videoRateMbps = 3.0;
+  double bottleneckMbps = 5.0;
+  uint32_t delayMs = 20;
+  double crossFlowMbps = 0.0;
+
+  CommandLine cmd(__FILE__);
+  cmd.AddValue("simTime", "Simuleringstid (sek)", simTime);
+  cmd.AddValue("videoRateMbps", "Videobitrate (Mbit/s)", videoRateMbps);
+  cmd.AddValue("bottleneckMbps", "Länkbandbredd (Mbit/s)", bottleneckMbps);
+  cmd.AddValue("delayMs", "Fördröjning (ms)", delayMs);
+  cmd.AddValue("crossFlowMbps", "Bakgrundstrafikens styrka (Mbps)", crossFlowMbps);
+  cmd.Parse(argc, argv);
+
+  NodeContainer nodes;
+  nodes.Create(2);
+
+  PointToPointHelper p2p;
+  p2p.SetDeviceAttribute("DataRate", StringValue(std::to_string(bottleneckMbps) + "Mbps"));
+  p2p.SetChannelAttribute("Delay", StringValue(std::to_string(delayMs) + "ms"));
+  NetDeviceContainer devices = p2p.Install(nodes);
+
+  InternetStackHelper stack;
+  stack.Install(nodes);
+  Ipv4AddressHelper address;
+  address.SetBase("10.1.1.0", "255.255.255.0");
+  Ipv4InterfaceContainer interfaces = address.Assign(devices);
+
+  uint16_t port = 4000;
+  UdpServerHelper server(port);
+  ApplicationContainer serverApp = server.Install(nodes.Get(1));
+  serverApp.Start(Seconds(0.0));
+  serverApp.Stop(Seconds(simTime));
+
+  double rateBps = videoRateMbps * 1e6;
+  double interval = (packetSize * 8) / rateBps;
+  UdpClientHelper client(interfaces.GetAddress(1), port);
+  client.SetAttribute("MaxPackets", UintegerValue(1000000));
+  client.SetAttribute("Interval", TimeValue(Seconds(interval)));
+  client.SetAttribute("PacketSize", UintegerValue(packetSize));
+  ApplicationContainer clientApp = client.Install(nodes.Get(0));
+  clientApp.Start(Seconds(0.5));
+  clientApp.Stop(Seconds(simTime));
+
+  if (crossFlowMbps > 0.0) {
+    std::cout << "Bakgrundstrafik aktiverad: " << crossFlowMbps << " Mbps" << std::endl;
+    OnOffHelper onoff("ns3::UdpSocketFactory", InetSocketAddress(interfaces.GetAddress(1), 5001));
+    onoff.SetAttribute("OnTime", StringValue("ns3::ConstantRandomVariable[Constant=1]"));
+    onoff.SetAttribute("OffTime", StringValue("ns3::ExponentialRandomVariable[Mean=0.2]"));
+    onoff.SetAttribute("PacketSize", UintegerValue(1000));
+    onoff.SetAttribute("DataRate", StringValue(std::to_string(crossFlowMbps) + "Mbps"));
+    ApplicationContainer crossApp = onoff.Install(nodes.Get(0));
+    crossApp.Start(Seconds(1.0));
+    crossApp.Stop(Seconds(simTime));
+    PacketSinkHelper sink("ns3::UdpSocketFactory", InetSocketAddress(interfaces.GetAddress(1), 5001));
+    ApplicationContainer sinkApp = sink.Install(nodes.Get(1));
+    sinkApp.Start(Seconds(0.5));
+    sinkApp.Stop(Seconds(simTime));
+  }
+
+  FlowMonitorHelper flowmon;
+  Ptr<FlowMonitor> monitor = flowmon.InstallAll();
+
+  Simulator::Stop(Seconds(simTime));
+  Simulator::Run();
+
+  monitor->CheckForLostPackets();
+  Ptr<Ipv4FlowClassifier> classifier = DynamicCast<Ipv4FlowClassifier>(flowmon.GetClassifier());
+  std::map<FlowId, FlowMonitor::FlowStats> stats = monitor->GetFlowStats();
+
+  for (auto &flow : stats) {
+    Ipv4FlowClassifier::FiveTuple t = classifier->FindFlow(flow.first);
+    if (t.destinationPort == 4000) {
+      std::cout << "==== RESULTAT ====" << std::endl;
+      std::cout << "Källa: " << t.sourceAddress << "  -->  Mottagare: " << t.destinationAddress << std::endl;
+      std::cout << "Paket skickade: " << flow.second.txPackets << std::endl;
+      std::cout << "Paket mottagna: " << flow.second.rxPackets << std::endl;
+      std::cout << "Förlust (%): " << (100.0 * (flow.second.txPackets - flow.second.rxPackets) / flow.second.txPackets) << std::endl;
+      std::cout << "Medelfördröjning (ms): " << (flow.second.delaySum.GetSeconds() * 1000 / flow.second.rxPackets) << std::endl;
+      std::cout << "Genomsnittlig jitter (ms): " << (flow.second.jitterSum.GetSeconds() * 1000 / (flow.second.rxPackets - 1)) << std::endl;
+      std::cout << "===================" << std::endl;
+    }
+  }
+
+  Simulator::Destroy();
+  return 0;
+}
